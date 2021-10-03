35883bc5cc102d17a0f12b96572baeccdc5669e9
小明 <youshaojunde@163.com>
2021-10-03 21:28:14 +0800

    feat: 使用网页模板

--- flask_demo/app.py
+++ flask_demo/app.py
@@ -1,12 +1,64 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/404')
+def not_found():
+    return render_template("404.html")
+
+
+@app.route('/blank')
+def blank():
+    return render_template("blank.html")
+
+
+@app.route('/buttons')
+def buttons():
+    return render_template("404.html")
+
+
+@app.route('/cards')
+def cards():
+    return render_template("404.html")
+
+
+@app.route('/charts')
+def charts():
+    return render_template("404.html")
+
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/tables')
+def tables():
+    return render_template("tables.html")
+
+
+@app.route('/utilities-animation')
+def utilities_animation():
+    return render_template("utilities-animation.html")
+
+
+@app.route('/utilities-border')
+def utilities_border():
+    return render_template("utilities-border.html")
+
+
+@app.route('/utilities-color')
+def utilities_color():
+    return render_template("utilities-color.html")
+
+
+@app.route('/utilities-other')
+def utilities_other():
+    return render_template("utilities-other.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    # TODO  [ECharts, WordCloud]
+    app.run(debug=True)
